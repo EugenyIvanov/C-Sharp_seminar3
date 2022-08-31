da00978b12b2742e74107e492fe0f53502c346dc
Евгений <ivanychcos@gmail.com>
2022-08-31 14:21:26 +0300

    add example 018

---
+++ Example018/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите значение четверти: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n == 1) Console.WriteLine("X > 0; Y > 0");
+else if (n == 2) Console.WriteLine("X < 0; Y > 0");
+else if (n == 3) Console.WriteLine("X < 0; Y < 0");
+else if (n == 4) Console.WriteLine("X > 0; Y < 0");
+else Console.WriteLine("Существует 4 четверти");
